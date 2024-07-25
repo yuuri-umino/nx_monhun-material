8511f53c05d0af25470d8fcbf094d230d3066ce4
海野 優里 <>
2024-07-25 18:05:24 +0900

    スラアクのデータ作成途中

--- src/utils/slashAx.ts
+++ src/utils/slashAx.ts
@@ -5,7 +5,28 @@ const greatsword: WeaponType = {
   derivations: [
     {
       name: '防衛隊系',
-      weapons: [],
+      weapons: [
+        {
+          name: '防衛隊変形式剣斧Ⅰ',
+          materials: [{ name: '鉄鉱石', quantity: 1 }],
+        },
+        {
+          name: '防衛隊変形式剣斧Ⅱ',
+          materials: [{ name: '蛮顎竜の鱗', quantity: 1 }],
+        },
+        {
+          name: '防衛隊変形式剣斧Ⅲ',
+          materials: [{ name: '勇気の証', quantity: 1 }],
+        },
+        {
+          name: '防衛隊変形式剣斧Ⅳ',
+          materials: [{ name: '桜火竜の上鱗', quantity: 1 }],
+        },
+        {
+          name: '防衛隊変形式剣斧Ⅴ',
+          materials: [{ name: '不滅の龍鱗', quantity: 1 }],
+        },
+      ],
     },
     {
       name: '鉱石素材系',
