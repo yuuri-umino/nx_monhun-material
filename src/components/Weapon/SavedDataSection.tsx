a010b2e9c7fd5448bfbbdda161a8aacebf64fa68
海野 優里 <>
2024-08-02 20:54:38 +0900

    保存の削除ボタンをスタイルのみ記述

--- src/components/Weapon/SavedDataSection.tsx
+++ src/components/Weapon/SavedDataSection.tsx
@@ -15,17 +15,19 @@ const SavedDataSection: React.FC<SavedDataSectionProps> = ({
   return (
     <Section>
       <h2 className="toppan">保存した計算結果</h2>
-      <ul className="d-flex flex-column flex-md-row">
+      <ul>
         {savedResults.map((result, index) => (
           <li key={index}>
+            <button className="delete-savedata">DELETE</button>
             <p className="vdl-shadow mb-1">{result.name}</p>
             <ul className="d-flex flex-column">
               {Object.entries(result.results).map(
                 ([materialName, quantity]) => (
                   <li className="mb-1" key={materialName}>
-                    {materialName}: [自分が今持っている個数:{' '}
-                    {result.ownedQuantities[materialName]} / 必要個数:{' '}
-                    {quantity}]
+                    <span className="toppan">{materialName}</span>　
+                    <br className="d-block d-md-none" />
+                    所持数: {result.ownedQuantities[materialName]} / 必要個数:{' '}
+                    {quantity}
                   </li>
                 )
               )}
@@ -49,11 +51,29 @@ const Section = styled.section`
     padding: 0;
     list-style: none;
     li {
+      position: relative;
       margin-bottom: 10px;
+      background-color: #fffbe8;
+      padding: 10px 20px;
+      border-radius: 6px;
+      .delete-savedata {
+        position: absolute;
+        top: 6%;
+        right: 4%;
+        padding: 0 10px;
+        border: 1px solid #a77d00;
+        border-radius: 20px;
+        background-color: #fff;
+        font-size: 14px;
+        font-weight: bold;
+        z-index: 10;
+      }
       p {
         color: #a77d00;
       }
       ul {
+        margin: 0;
+        padding: 0;
         li {
           color: #6f6f6f;
         }
@@ -70,18 +90,13 @@ const Section = styled.section`
     margin-top: 40px;
     padding-top: 40px;
     ul {
-      justify-content: space-between;
-      &::after {
-        content: '';
-        display: block;
-        width: 31%;
-      }
       li {
-        margin-bottom: 10px;
-        width: 31%;
-        background-color: #fffbe8;
-        padding: 10px;
-        border-radius: 6px;
+        margin-bottom: 20px;
+        padding: 10px 20px;
+        .delete-savedata {
+          top: 10%;
+          right: 2%;
+        }
         ul {
           li {
             width: 100%;
