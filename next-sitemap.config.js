0e4a8f89175a153590cdf2c91d355e8524fa4bc7
海野優里 <umino1.hanabiya@gmail.com>
2024-08-24 13:23:08 +0900

    GTMの修正等

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,3 @@
 /** @type {import('next-sitemap').IConfig} */
-export const siteUrl = process.env.SITE_URL || 'https://www.mh-material-calculate.net/weapon';
+export const siteUrl = process.env.SITE_URL || 'https://www.mh-material-calculate.net';
 export const generateRobotsTxt = true;
--- src/components/util/google-tag-manager.tsx
+++ src/components/util/google-tag-manager.tsx
@@ -29,17 +29,6 @@ const GoogleTagManager: FC<Props> = ({ googleTagManagerId }) => {
         }}
       />
       {/* End Google Tag Manager */}
-
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
-      {/* End Google Tag Manager (noscript) */}
     </>
   )
 }
